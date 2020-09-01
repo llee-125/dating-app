eb528a8717110ab3482beb5ff9dd2f85d7319a45
jimMehta <jimit1@gmail.com>
2020-08-31 20:13:22 -0700

    working seed + schema design

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,142 +1,41 @@
 let mongoose = require("mongoose");
-let db = require("../models/fitness");
+let db = require("../models/Profile");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost/profile", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
 
-let workoutSeed = [
+let profileSeed = [
   {
-    day: new Date().setDate(new Date().getDate() - 10),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bicep Curl",
-        duration: 20,
-        weight: 100,
-        reps: 10,
-        sets: 4,
-      },
-    ],
-  },
-  {
-    day: new Date().setDate(new Date().getDate() - 9),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Lateral Pull",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4,
-      },
-    ],
-  },
-  {
-    day: new Date().setDate(new Date().getDate() - 8),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Push Press",
-        duration: 25,
-        weight: 185,
-        reps: 8,
-        sets: 4,
-      },
-    ],
-  },
-  {
-    day: new Date().setDate(new Date().getDate() - 7),
-    exercises: [
-      {
-        type: "cardio",
-        name: "Running",
-        duration: 25,
-        distance: 4,
-      },
-    ],
-  },
-  {
-    day: new Date().setDate(new Date().getDate() - 6),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 285,
-        reps: 10,
-        sets: 4,
-      },
-    ],
-  },
-  {
-    day: new Date().setDate(new Date().getDate() - 5),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4,
-      },
-    ],
-  },
-  {
-    day: new Date().setDate(new Date().getDate() - 4),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Quad Press",
-        duration: 30,
-        weight: 300,
-        reps: 10,
-        sets: 4,
-      },
-    ],
-  },
-  {
-    day: new Date().setDate(new Date().getDate() - 3),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4,
-      },
-    ],
-  },
-  {
-    day: new Date().setDate(new Date().getDate() - 2),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Military Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4,
-      },
-    ],
-  },
-  {
-    day: new Date().setDate(new Date().getDate() - 1),
-    exercises: [
-      {
-        type: "cardio",
-        name: "Running",
-        duration: 25,
-        distance: 4,
-      },
-    ],
+    age: 22,
+    body_type: "a little extra",
+    diet: "strictly anything",
+    drinks: "socially",
+    drugs: "never",
+    education: "working on college/university",
+    essay:
+      "about me:i would love to think that i was some some kind of intellectual: either the dumbest smart guy, or the smartest dumb guy. cant say i can tell the difference. i love to talk about ideas and concepts. i forge odd metaphors instead of reciting cliches. like the similarities between a friend of mines house and an underwater salt mine. my favorite word is salt by the way (weird choice i know). to me most things in life are better as metaphors. i seek to make myself a little better everyday, in some productively lazy way. got tired of tying my shoes. considered hiring a five year old, but would probably have to tie both of our shoes... decided to only wear leather shoes dress shoes. about you: you love to have really serious, really deep conversations about really silly stuff. you have to be willing to snap me out of a light hearted rant with a kiss. you dont have to be funny, but you have to be able to make me laugh. you should be able to bend spoons with your mind, and telepathically make me smile while i am still at work. you should love life, and be cool with just letting the wind blow. extra points for reading all this and guessing my favorite video game (no hints given yet). and lastly you have a good attention span.",
+    ethnicity: "asian, white",
+    height: 75,
+    income: -1,
+    job: "transportation",
+    location: "south san francisco, california",
+    offspring: "doesnt have kids, but might want them",
+    orientation: "straight",
+    pets: "likes dogs and likes cats",
+    religion: "agnosticism and very serious about it",
+    sex: "men",
+    sign: "gemini",
+    smokes: "sometimes",
+    speaks: "english",
+    status: "single",
+    profile_image: "https://randomuser.me/api/portraits/men/1.jpg",
   },
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+db.Profile.deleteMany({})
+  .then(() => db.Profile.collection.insertMany(profileSeed))
   .then((data) => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);

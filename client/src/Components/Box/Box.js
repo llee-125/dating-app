08499d8630190548cd78ab93fefef99ded94958b
Meianna Oeser <meianna.oeser@gmail.com>
2020-09-10 09:50:24 -0700

    deleted unused components

--- client/src/Components/Box/Box.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { spacing, palette } from "@material-ui/system";
-import Box from "@material-ui/core/Box";
-
-<Box /> = styled.div`
-  ${spacing}${palette}
-`;
-
-export default Box;

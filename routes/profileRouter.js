ba21a9528f3287b3f9a0f1e10f724c919d38fc05
jimMehta <jimit1@gmail.com>
2020-09-12 16:04:44 -0700

    working profileRouter

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,10 +1,25 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth");
 let db = require("../models/profileModel");
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+let userId = "";
 
 router.get("/test", (req, res) => {
-  res.send("working profileRouter");
+  res.send("Hello, test is working");
 });
+
+// router.get("/test2", (req, res) => {
+//   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+//   res.json({
+//     token,
+//     user: {
+//       id: user._id,
+//       email: user.email,
+//     },
+//   });
+// });
 
 router.get("/all", (req, res) => {
   db.Profile.find().then((profiles) => res.send(profiles));
@@ -28,23 +43,23 @@ router.patch("/edit/:id", (req, res) => {
   );
 });
 
-router.post("/", auth, async (req, res) => {
-  try {
-    const { title } = req.body;
+// router.post("/", auth, async (req, res) => {
+//   try {
+//     const { title } = req.body;
 
-    if (!title)
-      return res.status(400).json({ msg: "Not all fields have been entered." });
+//     if (!title)
+//       return res.status(400).json({ msg: "Not all fields have been entered." });
 
-    const newTodo = new Todo({
-      title,
-      userId: req.user,
-    });
-    const savedTodo = await newTodo.save();
-    res.json(savedTodo);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+//     const newTodo = new Todo({
+//       title,
+//       userId: req.user,
+//     });
+//     const savedTodo = await newTodo.save();
+//     res.json(savedTodo);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
 // router.get;
 

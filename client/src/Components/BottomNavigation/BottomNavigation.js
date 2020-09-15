09b44b84ada36d39d432febee34ce057d9d872b0
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-09-14 17:52:55 -0700

    changed BottomNavigation and added Menu item

--- client/src/Components/BottomNavigation/BottomNavigation.js
+++ client/src/Components/BottomNavigation/BottomNavigation.js
@@ -1,37 +1,50 @@
-// import { SvgIcon } from "@material-ui/core";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-// import Icon from "@material-ui/core/Icon";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import { makeStyles } from "@material-ui/core/styles";
+import { Restore } from "@material-ui/icons";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import FolderIcon from "@material-ui/icons/Folder";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import RestoreIcon from "@material-ui/icons/Restore";
 import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Image from "react-bootstrap/Image";
 
-import SvgMumble from "../MumbleIcon/Mumble";
+import MumblePng from "./mumble-logo.png";
+import SvgMumble from "./MumbleIcon/Mumble";
 
 // import MumbleIcon from "./Mumble.svg";
 
 const useStyles = makeStyles({
   root: {
-    width: 500,
+    width: "100%",
+    position: "fixed",
+    bottom: 0,
   },
-  // imageIcon: {
-  //   height: "100%",
-  //   width: "100%",
-  // },
-  // iconRoot: {
-  //   textAlign: "center",
-  // },
+  smallImg: {
+    width: "30px",
+    height: "30px",
+  },
 });
 
 export default function LabelBottomNavigation() {
   const classes = useStyles();
   const [value, setValue] = React.useState("recents");
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+  };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
@@ -49,10 +62,39 @@ export default function LabelBottomNavigation() {
         label="Recents"
         value="recents"
         icon={<RestoreIcon />}
-      />
+      />{" "}
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>You Like</MenuItem>
+        <MenuItem onClick={handleClose}>Mutual Like</MenuItem>
+      </Menu>
+      {/* <DropdownButton
+        drop="up"
+        key="right"
+        title={
+          <div>
+            <Image
+              className={classes.smallImg}
+              src={MumblePng}
+              thumbnail
+              fluid={true}
+              // src="https://via.placeholder.com/150"
+              alt="icon"
+            />
+          </div>
+        }
+      >
+        <Dropdown.Item eventKey="1">Likes You</Dropdown.Item>
+      </DropdownButton> */}
       <BottomNavigationAction
         label="Likes"
         value="likes"
+        onClick={handleClick}
         icon={<FavoriteIcon />}
       />
       <BottomNavigationAction

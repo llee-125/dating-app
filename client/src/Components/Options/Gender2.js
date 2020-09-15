d5ca356ee7b351b5375045b574c827e815aa1155
Meianna Oeser <meianna.oeser@gmail.com>
2020-09-14 20:37:02 -0700

    adding onclicks and handle functions

---
+++ client/src/Components/Options/Gender2.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+const Gender2 = () => {
+  return (
+    <div style={{ margin: "0px 0px 10px 0px" }}>
+      <Button variant="contained" color="primary" disableElevation>
+        Male
+      </Button>
+    </div>
+  );
+};
+
+export default Gender2;

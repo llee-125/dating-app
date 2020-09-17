49425e6a967a9bd6ecc534996782e1a594b495a4
llee-125 <llee@berkeley.edu>
2020-09-17 13:54:29 -0700

    Redirect login to profile and signup to profile/discover

--- client/src/Components/auth/Register.js
+++ client/src/Components/auth/Register.js
@@ -29,7 +29,7 @@ export default function Register() {
         user: loginRes.data.user,
       });
       localStorage.setItem("auth-token", loginRes.data.token);
-      history.push("/");
+      history.push("/profile/discover");
     } catch (err) {
       err.response.data.msg && setError(err.response.data.msg);
     }
